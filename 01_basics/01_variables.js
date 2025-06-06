1cc564db045c08322ac663472df1aefbf10cfa09
Akash <imakashh09@gmail.com>
2025-06-06 10:41:20 +0000

    Varables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 126345
+let accountEmail = "akash@yahoo.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // Not allowed
+/*
+perfer not to use var
+becouse of issue in block scope and functional scope
+*/
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity]);
