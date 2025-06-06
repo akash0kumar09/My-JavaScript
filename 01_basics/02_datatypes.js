0178d0e173949dd51e3fb57f9b2475d4416806db
Akash <imakashh09@gmail.com>
2025-06-06 10:41:37 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all JS code as newer version
+
+// alert(9+8) // we are using node js, not browser
+
+console.log(3+3)
+
+console.log("akash");// code should be readable 
+
+
+let name = "akash"
+let age = 23
+let IsLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+//object
+
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined
+
