94016b7b2a7b867c27f12b9aae3d068742bdf2e2
Akash <imakashh09@gmail.com>
2025-06-08 12:31:38 +0000

    Datatypes Summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types : String, Number, Boolearn, Undefined, null, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 23455647456467n
+
+// Refernce (Not Primitive)
+
+// array, objects, Funtions
+
+const heros = ["Captainamerica", "Spiderman"]
+let myObj ={
+    name: "Vanshita",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros);
